63ffb6ac73bcefd4ad40e29780e48d13739de813
mogbojuri segun <ademegcollege@gmail.com>
2025-07-16 10:23:40 +0100

    Add Counter component with useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,13 +1,9 @@
-import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <>
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
+      <Counter />
     </>
   );
 }
