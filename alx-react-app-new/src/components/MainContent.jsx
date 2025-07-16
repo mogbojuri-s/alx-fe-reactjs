7cae750bba7712b8c43e4702df406ad77da83ee1
mogbojuri segun <ademegcollege@gmail.com>
2025-07-16 10:17:33 +0100

    Apply inline styles to components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
